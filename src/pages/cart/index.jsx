3bebb1a6ae7ea8a52a8929f08a66d701e9c6366d
Dutra PC <joao.vitornl@gmail.com>
2024-02-18 16:19:30 -0300

    :construction: Create cart component and editor

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -1,15 +1,67 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useEffect, useState }from 'react';
+import { AiOutlineDelete } from "react-icons/ai";
+import { toast } from 'react-toastify';
 
+
+import Loading from '../../components/loading';
 import Navinfo from '../../components/navinfo';
+import cartAction from '../../actions/category';
 import Layout from '../../components/layout';
+import CartEditor from './editor';
+import { Container, Box, TextContainer, IconContainer } from './styles';
 
 const Cart = () => {
+  const [cart, setCart] = useState(null);
+  const [editor, setEditor] = useState(null);
+
+  const getCart = async () => {
+    const response = await cartAction.get();
+    if (response.error){
+        return toast.error("Erro ao carregar carrinho.");
+    }
+    setCart(response);
+  }
+
+  const deleteCart = async (id) => {
+    const send = async () => {
+      const response =  await cartAction.remove({id});
+      if (response.error) throw error;
+      return getCart();
+    }
+    toast.promise(send(), {
+      pending: `apagando item`,
+      success: `item apagado com sucesso`,
+      error: `erro ao apagar item`
+    })
+  }
+
+  useEffect(() => {
+    getCart()
+  }, [])
+
+  if (!cart) return <Loading layout/>
+  if (editor != null) return <CartEditor data={cart} id={editor != true ? editor : null} onBack={() => setEditor(null) & getCart()}/> 
 
   return (
     <Layout>
-      <Navinfo name={"carrinho"} subname={"items"} buttonName={"adicionar"} size={1}/>
-
+      <Navinfo name={"Carrinho"} subname={"items"} buttonName={"adicionar"} size={cart.length || 0} onButton={() => setEditor(true)}/>
+      <Container>
+          {
+            cart.map((item, index) => {
+              return (
+                <Box key={index}>
+                  <TextContainer  onClick={() => setEditor(item._id)}>
+                    <h3>{item.name}</h3>
+                    <p>{item.description ? item.description : "sem descrição."}</p>
+                  </TextContainer>
+                  <IconContainer> 
+                    <AiOutlineDelete class="icon" size={30} onClick={() => deleteCart(item._id)}/>
+                  </IconContainer>
+                </Box>
+              )
+            })
+          }
+      </Container>
     </Layout>
   );
 }
